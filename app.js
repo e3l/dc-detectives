ae3a2b22ac515d197c2260fd894deafc6eda36de
MushroomYolo <67622438+MushroomYolo@users.noreply.github.com>
2021-02-13 17:23:25 -0800

    Added tools for About Us page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ordersRouter = require('./routes/orders');
-
+var aboutRouter = require('./routes/aboutus');
 var app = express();
 
 // view engine setup
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/orders', ordersRouter);
+app.use('/aboutus', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
